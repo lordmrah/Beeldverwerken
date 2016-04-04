e090bc264fdae37c0e560f805949490f54e09af9
Harm Manders <harmmanders@gmail.com>
2016-04-04 16:43:57 +0200

    gitignore toegevoegd en opdracht 3 bijna af

--- Huiswerk 1/e1_jonas_vanOenen_harm_manders.m
+++ Huiswerk 1/e1_jonas_vanOenen_harm_manders.m
@@ -12,10 +12,24 @@ function e1_jonas_vanOenen_harm_manders()
 %     plot ( profile ( a , 100 , 100 , 120 , 120 , 200 , 'nearest') , 'r' );
 %     hold off ;
     
-    %% Question 3.1.1
+    %% Question 3.1.1 + 3.1.2 + 3.1.3
     a = imread('cameraman.tif');
-    image = rotateImage(a, 30, 'linear');
+    image = rotateImage(a, 45, 'linear');
     imshow(image);
+    
+    % Question 3.1.4
+    a = imread('cameraman.tif');
+    tic
+    nearImg = rotateImage(a, 30, 'nearest');
+    disp('nearest:')
+    toc
+    tic
+    linImg = rotateImage(a, 30, 'linear');
+    disp('linear:')
+    toc
+    imshow(nearImg);
+    imshow(linImg);
+    
     
     %% Question 4.1
 %     plotParallelogram(0,0,0,0,1,1)
@@ -52,9 +66,10 @@ end
 
 function rotatedImage = rotateImage ( image , angle , method )
     angle = degtorad(angle);
+    image = addBorder(image, angle);
     
     % Create the necessary rotation matrix
-    rotationMatrix = [cos(angle),sin(angle);-sin(angle),cos(angle)]
+    rotationMatrix = [cos(angle),sin(angle);-sin(angle),cos(angle)];
     
     % Obtain indices needed for interpolation
     [imSizeX, imSizeY] = size(image);
@@ -78,6 +93,50 @@ function rotatedImage = rotateImage ( image , angle , method )
     end
     
     rotatedImage = reshape(rotatedImage, imSizeY, imSizeX);
+end
+
+function borderedImage = addBorder(image, angle)
+    % Create the necessary rotation matrix
+    rotationMatrix = [cos(angle),sin(angle);-sin(angle),cos(angle)];
+    
+    % Obtain indices needed for interpolation
+    [imSizeX, imSizeY] = size(image);
+    center = [(imSizeX+1)/2;(imSizeY+1)/2];
+    
+    RBM = [rotationMatrix, center-rotationMatrix*center;0,0,1];
+    
+    cornerMatrix = [1,1,imSizeX,imSizeX;1,imSizeY,1,imSizeY;1,1,1,1];
+    rotCornM = RBM*cornerMatrix;
+    Mcell = mat2cell(rotCornM,repmat(1,3,1),4);
+    Xmax = max(Mcell{1});
+    Xmin = min(Mcell{1});
+    Ymax = max(Mcell{2});
+    Ymin = min(Mcell{2});
+    maxDiff = 0;
+    if 1 - Xmin > maxDiff
+        maxDiff = 1 - Xmin;
+    end
+    if 1 - Ymin > maxDiff
+        maxDiff = 1 - Ymin;
+    end
+    if Xmax - imSizeX > maxDiff
+        maxDiff = Xmax - imSizeX;
+    end
+    if Ymax - imSizeY > maxDiff
+        maxDiff = Xmax - imSizeY;
+    end
+    maxDiff = ceil(maxDiff);
+    
+    corBor = zeros(maxDiff,maxDiff);
+    verBor = zeros(imSizeX,maxDiff);
+    horBor = zeros(maxDiff,imSizeY);
+    
+    borderedImage = [corBor, horBor, corBor;
+                    verBor, image, verBor;
+                    corBor, horBor, corBor];
+    
+    
+    
 end
     
 function color = pixelValue( image , x, y, method )

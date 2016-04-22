921f992f50733a8a9b99dce039d47a0a23631ad6
Harm Manders <harmmanders@gmail.com>
2016-04-22 16:18:00 +0200

    opgave 2

--- Homework 2/main.m
+++ Homework 2/main.m
@@ -2,49 +2,42 @@ function main()
    
     %% Question 1
     img = imread('cameraman.jpg');
-    
+    SPM = 4;
+    SPN = 3;
     % Motion blur
-    subplot(4,2,1)
+    subplot(SPM,SPN,[1,4]);
     imshow(img)
-    title('Q1.1 Original')
+    title('Original')
     G = (1/6)*[1,1,1,1,1,1];
     image = filterImage(img, G);
-    subplot(4,2,2);
+    subplot(SPM,SPN,2);
     imshow(image);
     title('Q1.1 Motion blur')
     
     % Optical blur
-    subplot(4,2,3)
-    imshow(img)
-    title('Q1.2 Original')
     n = 7;
     G = fspecial('gaussian', [7 7], 3)
 %     G = (1/9)*[1,1,1;1,1,1;1,1,1];
     image = filterImage(img, G);
-    subplot(4,2,4);
+    subplot(SPM,SPN,3);
     imshow(image);
     title('Q1.2 Optical blur')
     
     % Unsharp masking
-    subplot(4,2,5)
-    imshow(img)
-    title('Q1.3 Original')
     n = 10;
     G = (1/n^2)*ones(n);
 %     G = (1/6)*[1,1,1,1,1,1];
     blurImage = filterImage(img, G);
     image2 = img+(img-blurImage);
-    subplot(4,2,6);
+    subplot(SPM,SPN,5);
     imshow(image2);
     title('Q1.3 Unsharp masking')
    
     %% Exercise 2
-    subplot(4,2,7)
-    imshow(img)
-    title('Q2.1 Original')
     G = Gauss(3)
+    %Q2.1
     image = filterImage(img, G);
-    subplot(4,2,8);
+    subplot(SPM,SPN,6)
     imshow(image);
     title('Q2.1 Gaussian')
 
@@ -62,9 +55,55 @@ function main()
        return
     end
 
-
-    Gauss(3)
+    %Q2.2
+    sum(sum(Gauss(3)))
+    
+    %2.3
+    subplot(SPM,SPN,7);
     mesh(Gauss(3))
+    title('Q2.3 mesh')
+    %2.4
+    % pixels
+    %2.5
+    data = benchmark(1,5,10);
+    subplot(SPM,SPN,[8,9]);
+    scatter(data(:,1),data(:,2),15,linspace(1,10,length(data(:,1))),'filled')
+    function data = benchmark(minS,maxS,N)
+        img = imread('cameraman.jpg');
+        data = zeros((maxS-minS+1)*N,2);
+        count = 0;
+        for sigma=minS:maxS,
+            
+            for j=1:N,
+                tic;
+                imfilter(img, Gauss(sigma), 'conv','replicate');
+                time = toc;
+                data(count*N+j,:) = [sigma,time];
+            end
+            count = count + 1;
+        end
+    end
+    %2.6
+    % O(n)
+    %2.7
+    img = imread('cameraman.jpg');
+    b1 = imfilter(img, Gauss(1), 'conv','replicate');
+    b1 = imfilter(b1, Gauss(1), 'conv','replicate');
+    
+    b2 = imfilter(img, Gauss(2), 'conv','replicate');
+
+    subplot(SPM,SPN,10)
+    imshow(b1)
+    subplot(SPM,SPN,11)
+    imshow(b2)
+    eq = isequal(b1,b2)
+    subplot(SPM,SPN,12)
+    imshowpair(b1,b2,'diff');
+    
+    %imshow(diff)
+    
+    
+
     
     
 

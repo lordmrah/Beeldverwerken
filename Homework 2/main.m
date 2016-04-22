66ebdc09c303dc13670fe399f4d71526584e80f6
Jonas van Oenen <jonas.vanoenen@gmail.com>
2016-04-22 15:19:54 +0200

    Titles forever

--- Homework 2/main.m
+++ Homework 2/main.m
@@ -4,39 +4,50 @@ function main()
     img = imread('cameraman.jpg');
     
     % Motion blur
-    subplot(3,2,1)
+    subplot(4,2,1)
     imshow(img)
+    title('Q1.1 Original')
     G = (1/6)*[1,1,1,1,1,1];
     image = filterImage(img, G);
-    subplot(3,2,2);
+    subplot(4,2,2);
     imshow(image);
+    title('Q1.1 Motion blur')
     
     % Optical blur
-    subplot(3,2,3)
+    subplot(4,2,3)
     imshow(img)
+    title('Q1.2 Original')
     n = 7;
     G = fspecial('gaussian', [7 7], 3)
 %     G = (1/9)*[1,1,1;1,1,1;1,1,1];
     image = filterImage(img, G);
-    subplot(3,2,4);
+    subplot(4,2,4);
     imshow(image);
+    title('Q1.2 Optical blur')
     
     % Unsharp masking
-    subplot(3,2,5)
+    subplot(4,2,5)
     imshow(img)
+    title('Q1.3 Original')
     n = 10;
     G = (1/n^2)*ones(n);
 %     G = (1/6)*[1,1,1,1,1,1];
     blurImage = filterImage(img, G);
     image2 = img+(img-blurImage);
-    subplot(3,2,6);
+    subplot(4,2,6);
     imshow(image2);
-    
-    subplot(3,2,5)
+    title('Q1.3 Unsharp masking')
+   
+    %% Exercise 2
+    subplot(4,2,7)
     imshow(img)
-    
-    
- 
+    title('Q2.1 Original')
+    G = Gauss(3)
+    image = filterImage(img, G);
+    subplot(4,2,8);
+    imshow(image);
+    title('Q2.1 Gaussian')
+
     function filteredImage = filterImage(img, G)
         filteredImage = imfilter(img, G, 'conv', 'replicate');
     end
@@ -50,6 +61,8 @@ function main()
        gaussGrid = gaussGrid./factor;
        return
     end
+
+
     Gauss(3)
     mesh(Gauss(3))
     

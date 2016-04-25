6b57495dd81c1ac4ffaeb39980f9494acee27ddf
Jonas van Oenen <jonas.vanoenen@gmail.com>
2016-04-25 20:13:15 +0200

    Comments

--- Homework 2/main.m
+++ Homework 2/main.m
@@ -1,13 +1,17 @@
 function main()
-   
     %% Question 1
     img = imread('cameraman.jpg');
+    
+    % Subplot M and N
     SPM = 4;
     SPN = 3;
-    % Motion blur
+    
+    % Original image
     subplot(SPM,SPN,[1,4]);
     imshow(img)
     title('Original')
+    
+    % Motion blur
     G = (1/6)*[1,1,1,1,1,1];
     image = filterImage(img, G);
     subplot(SPM,SPN,2);
@@ -16,44 +20,29 @@ function main()
     
     % Optical blur
     n = 7;
-    G = fspecial('gaussian', [7 7], 3)
-%     G = (1/9)*[1,1,1;1,1,1;1,1,1];
+    G = fspecial('gaussian', [7 7], 3);
     image = filterImage(img, G);
     subplot(SPM,SPN,3);
     imshow(image);
     title('Q1.2 Optical blur')
     
     % Unsharp masking
-    n = 10;
+    n = 3;
     G = (1/n^2)*ones(n);
-%     G = (1/6)*[1,1,1,1,1,1];
     blurImage = filterImage(img, G);
     image2 = img+(img-blurImage);
     subplot(SPM,SPN,5);
     imshow(image2);
     title('Q1.3 Unsharp masking')
    
-    %% Exercise 2
-    G = Gauss(3)
+    % Question 2
+    
     %Q2.1
+    G = Gauss(3);
     image = filterImage(img, G);
     subplot(SPM,SPN,6)
     imshow(image);
     title('Q2.1 Gaussian')
-
-    function filteredImage = filterImage(img, G)
-        filteredImage = imfilter(img, G, 'conv', 'replicate');
-    end
-
-    function gaussGrid= Gauss(sigma)
-       [X,Y] = meshgrid(-sigma:sigma, -sigma:sigma);
-       
-       dist = sqrt(X.*X+Y.*Y);
-       gaussGrid = 1/((sqrt(2*pi)*sigma)^2)*exp(-(dist.^2)/(2*sigma^2));
-       factor = sum(sum(gaussGrid));
-       gaussGrid = gaussGrid./factor;
-       return
-    end
 
     %Q2.2
     sum(sum(Gauss(3)))
@@ -87,10 +76,10 @@ function main()
     % O(n)
     %2.7
     img = imread('cameraman.jpg');
-    b1 = imfilter(img, Gauss(1), 'conv','replicate');
-    b1 = imfilter(b1, Gauss(1), 'conv','replicate');
+    b1 = imfilter(img, Gauss(2), 'conv','replicate');
+    b1 = imfilter(b1, Gauss(2), 'conv','replicate');
     
-    b2 = imfilter(img, Gauss(2), 'conv','replicate');
+    b2 = imfilter(img, Gauss(sqrt(2^2+2^2)), 'conv','replicate');
 
     subplot(SPM,SPN,10)
     imshow(b1)
@@ -102,17 +91,18 @@ function main()
     
     %imshow(diff)
     
-    
+        function filteredImage = filterImage(img, G)
+        filteredImage = imfilter(img, G, 'conv', 'replicate');
+    end
 
-    
-    
-
-
-
-
-
-
-
-
+    function gaussGrid= Gauss(sigma)
+       [X,Y] = meshgrid(-sigma:sigma, -sigma:sigma);
+       
+       dist = sqrt(X.*X+Y.*Y);
+       gaussGrid = 1/((sqrt(2*pi)*sigma)^2)*exp(-(dist.^2)/(2*sigma^2));
+       factor = sum(sum(gaussGrid));
+       gaussGrid = gaussGrid./factor;
+       return
+    end
 
 end

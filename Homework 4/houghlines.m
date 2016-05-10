9bb62cb2251da8f62c5272b99326b24a5209d876
lucasdevries <lucasdevries@home.nl>
2016-05-10 14:55:23 +0200

    begint vgm te kloppen

--- Homework 4/houghlines.m
+++ Homework 4/houghlines.m
@@ -15,8 +15,10 @@ function houghlines(im, h, thresh)
 rows = size( im, 1 );
 columns = size( im, 2 );
 rhomax = sqrt ( rows ^2 + columns ^2);
-drho = size(h,1);
-dtheta = size(h,2);
+nrho = size(h,1)
+ntheta = size(h,2)
+% drho = 2*rhomax/(nrho-1)
+% dtheta = pi/ntheta
 
 % To make a binary matrix and finding the isolated regions of an image
 % using the threshold (thresh)
@@ -30,4 +32,7 @@ for n = 1:nregions
     region = mask .* h;
     [maxvalue1, Y] = max(region);
     [maxvalue2, X] = max(maxvalue1);
-    
+    XY = [X;Y];
+    theta(n) = ((XY(1,1)*pi)/(ntheta));
+    rho(n) = ((XY(2,1)*2*rhomax)/(nrho))-rhomax;
+end    

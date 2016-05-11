fa4d9ed618bba243a26fd6eff6602764b670b444
Harm <harmmanders@gmail.com>
2016-05-11 11:09:57 +0200

    finished opg2

---
+++ Homework 4/hough.m
@@ -0,0 +1,31 @@
+function h = hough(im, Thresh, nRho, nTheta)
+    BW = edge(im,'canny',Thresh);
+    
+%     [H,T,R] = hough(BW, 'RhoResolution', 0.5, 'ThetaResolution', 0.5);
+   
+    
+    [N,M] = size(im);
+    D = sqrt(M^2+N^2);
+    dRho = (2*D)/(nRho-1);
+    
+    dTheta = pi/nTheta;
+    thetas = [0:dTheta:(pi-dTheta)]
+    
+    h = zeros(nRho,nTheta);
+    size(h);
+    
+% Find non-zero pixels in binary edge image
+    [Y,X] = find(BW);
+    for k = 1:length(X),
+        x = X(k);
+        y = Y(k);
+        for i=1:length(thetas),
+            theta = thetas(i);
+            rho = x*sin(theta) - y*cos(theta);
+            rhoIndex = round(rho/dRho + nRho/2);
+            thetaIndex = round(theta/dTheta + 1);
+%             val  = accumMatrix(rI,tI);
+            h(rhoIndex,thetaIndex) = h(rhoIndex, thetaIndex) + 1;
+        end
+    end
+end
--- Homework 4/main.m
+++ Homework 4/main.m
@@ -6,40 +6,24 @@ function main()
     szel = imread('attachments/szeliski.png');
 % Choose what image to use
     RGB = shape;
+    ThreshMin = 0.1;
+    ThreshMax = 0.5;
+    nTheta = 1000;
+    nRho = 500;
+    
+    
     
     I = rgb2gray(RGB);
-    BW = edge(I,'canny');
+    BW = edge(I,'Canny',[ThreshMin,ThreshMax]);
+    h = hough(I,[ThreshMin,ThreshMax],nRho,nTheta);
     
-%     [H,T,R] = hough(BW, 'RhoResolution', 0.5, 'ThetaResolution', 0.5);
+%     size(accumMatrix)
+%     imshow(h,'XData',ThetaRange,'YData',RhoRange,'InitialMagnification','fit')
+    normH = h - min(h(:));
+    normH = normH ./ max(normH(:));
     
-    RhoRes = 0.5;
-    ThetaRes = 0.5;
-    ThetaRange = [0,180];
-    [N,M] = size(I);
-    D = sqrt(M^2+N^2);
-    RhoRange = [-D,D];
-    RhoVal = RhoRange / RhoRes;
-    ThetaVal = ThetaRange / ThetaRes;
-    accumMatrix = zeros(round(diff(RhoVal)),round(diff(ThetaVal))+1);
-    size(accumMatrix)
-% Find non-zero pixels in binary edge image
-    [Y,X] = find(BW);
-    for k = 1:length(X),
-        x = X(k);
-        y = Y(k);
-        for theta=ThetaRange(1):ThetaRes:ThetaRange(2),
-            rad = deg2rad(theta);
-            r = x*sin(rad) - y*cos(rad);
-            rI = round((r+RhoRange(2))/RhoRes);
-            tI = (theta + 1)/ThetaRes;
-%             val  = accumMatrix(rI,tI);
-            accumMatrix(rI,tI) =  1;
-        end
-    end
-    size(accumMatrix)
-    imshow(accumMatrix,'XData',ThetaRange,'YData',RhoRange,'InitialMagnification','fit')
-    colormap(hot)
-    xlabel('\theta'), ylabel('\rho');
+%     colormap(hot)
+    
 %     axis on, axis normal, hold on;
     
     subplot(2,2,1);
@@ -47,10 +31,9 @@ function main()
     subplot(2,2,2);
     imshow(BW)
     subplot(2,2,[3,4]);
-%     imshow(imadjust(mat2gray(accumMatrix)),'XData',T,'YData',R, 'InitialMagnification','fit');
-%     xlabel('\theta'), ylabel('\rho');
-%     axis on, axis normal, hold on;
-%     colormap(hot);
+    imshow(normH)
+%     colormap(hot)
+    xlabel('\theta'), ylabel('\rho');
     
     %%%% THEORY QUESTIONS %%%%%
     % 1.
